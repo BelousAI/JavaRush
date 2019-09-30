ebce95e6da57938929fe3611aa24c3c4c9a32ac1
Anton <belous.nsk@gmail.com>
2019-09-30 11:15:20 +0300

    Solution task3101.2

--- 4.JavaCollections/src/com/javarush/task/task31/task3101/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task31/task3101/Solution.java
@@ -1,8 +1,6 @@
 package com.javarush.task.task31.task3101;
 
 import java.io.*;
-import java.nio.file.*;
-import java.nio.file.attribute.BasicFileAttributes;
 import java.util.*;
 
 /*
@@ -15,22 +13,62 @@ public class Solution {
         File resultRenamed = new File(resultFileAbsolutePath.getParent() + "/allFilesContent.txt");
         FileUtils.renameFile(resultFileAbsolutePath, resultRenamed);
 
-        List<File> list = new ArrayList<>();
+        ArrayList<File> result;
+        result=vozvratFile(root);
 
-        FileUtils.searchFiles(root, list);
+        try(FileOutputStream fileOutputStream=new FileOutputStream(resultRenamed))
+        {
+            for (File file:result)
+            {
+                try(FileInputStream inputStream=new FileInputStream(file))
+                {
+                    while(inputStream.available()>0)
+                    {
+                        fileOutputStream.write(inputStream.read());
+                    }
+                    byte[]bytes=System.lineSeparator().getBytes();
+                    fileOutputStream.write(bytes);
+                }
 
-        list.sort(new Comparator<File>() {
-            @Override
-            public int compare(File o1, File o2) {
-                return (o1.getName().compareTo(o2.getName()));
             }
-        });
-
-        try (BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(resultRenamed, true))) {
-            FileUtils.copyFiles(list, out);
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+//        List<File> list = new ArrayList<>();
+//
+//        FileUtils.searchFiles(root, list);
+//
+//        list.sort(new Comparator<File>() {
+//            @Override
+//            public int compare(File o1, File o2) {
+//                return (o1.getName().compareTo(o2.getName()));
+//            }
+//        });
+//
+//        try (BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(resultRenamed, true))) {
+//            FileUtils.copyFiles(list, out);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+    }
+
+    public static ArrayList<File> vozvratFile(File path)
+    {
+        ArrayList<File>result=new ArrayList<>();
+        for (File file:path.listFiles())
+        {
+            if (file.isFile()&&file.length()<=50)
+            {
+                result.add(file);
+            }
+            if (file.isDirectory())
+            {
+                ArrayList<File>result1=vozvratFile(file);
+                result.addAll(result1);
+            }
+        }
+        return result;
     }
 }
