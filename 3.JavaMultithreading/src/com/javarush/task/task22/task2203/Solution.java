0dbddf25ec55ff203671299012d142a4323e3e20
Anton <belous.nsk@gmail.com>
2019-10-01 20:04:37 +0300

    Solution task2203

--- 3.JavaMultithreading/src/com/javarush/task/task22/task2203/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2203/Solution.java
@@ -4,8 +4,18 @@ package com.javarush.task.task22.task2203;
 Между табуляциями
 */
 public class Solution {
-    public static String getPartOfString(String string) {
-        return null;
+    public static String getPartOfString(String string) throws TooShortStringException {
+        if (string == null) {
+            throw new TooShortStringException();
+        }
+
+        String[] array = string.split("\t");
+
+        if (array.length < 3) {
+            throw new TooShortStringException();
+        }
+
+        return array[1];
     }
 
     public static class TooShortStringException extends Exception {
