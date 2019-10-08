2748fe0a28075a15f8c25181d6cee3d2daa8790f
Anton <belous.nsk@gmail.com>
2019-10-08 20:50:50 +0300

    Solution task2208

--- 3.JavaMultithreading/src/com/javarush/task/task22/task2208/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2208/Solution.java
@@ -1,5 +1,8 @@
 package com.javarush.task.task22.task2208;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 /* 
@@ -7,9 +10,27 @@ import java.util.Map;
 */
 public class Solution {
     public static void main(String[] args) {
+        Map<String, String> map = new LinkedHashMap<>();
+        map.put("name", "Ivanov");
+        map.put("country", "Ukraine");
+        map.put("city", "Kiev");
+        map.put("age", null);
+        map.put("Gritting", "Hello!");
 
+        System.out.println(getQuery(map));
     }
     public static String getQuery(Map<String, String> params) {
-        return null;
+        StringBuilder sb = new StringBuilder();
+        if (params == null || params.isEmpty()) return sb.toString();
+
+        for (Map.Entry<String, String> entry : params.entrySet()) {
+            if (entry.getKey() == null || entry.getValue() == null) {
+                continue;
+            }
+            sb.append(entry.getKey()).append(" = '").append(entry.getValue()).append("'").append(" and ");
+        }
+
+        sb.delete(sb.lastIndexOf("'") + 1, sb.length());
+        return sb.toString();
     }
 }
