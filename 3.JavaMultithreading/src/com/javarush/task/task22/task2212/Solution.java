970e0b8e59be102da8dbb0d96bd0a2d69ae845ab
Anton <belous.nsk@gmail.com>
2019-10-18 21:14:08 +0300

    Solution task2312

--- 3.JavaMultithreading/src/com/javarush/task/task22/task2212/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2212/Solution.java
@@ -5,10 +5,20 @@ package com.javarush.task.task22.task2212;
 */
 public class Solution {
     public static boolean checkTelNumber(String telNumber) {
-        return false;
+        if (telNumber != null && telNumber.matches("\\+?\\d{2}\\(?\\d{3}\\)?\\d-?\\d\\d-?\\d+-?\\d\\d?")) {
+            return true;
+        } else return false;
     }
 
     public static void main(String[] args) {
-
+//        System.out.println(checkTelNumber("+380501234567"));
+//        System.out.println(checkTelNumber("+38(050)1234567"));
+//        System.out.println(checkTelNumber("+38050123-45-67"));
+//        System.out.println(checkTelNumber("050123-4567"));
+//        System.out.println(checkTelNumber("+38)050(1234567"));
+//        System.out.println(checkTelNumber("+38(050)1-23-45-6-7"));
+//        System.out.println(checkTelNumber("050ххх4567"));
+//        System.out.println(checkTelNumber("050123456"));
+//        System.out.println(checkTelNumber("(0)501234567"));
     }
 }
