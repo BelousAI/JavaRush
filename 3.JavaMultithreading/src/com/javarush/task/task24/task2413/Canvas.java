f9284aebc0ff082def26aabff866405bb1a0ec5f
Anton <belous.nsk@gmail.com>
2019-10-23 21:03:51 +0300

    Solution task2413

---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Canvas.java
@@ -0,0 +1,55 @@
+package com.javarush.task.task24.task2413;
+
+public class Canvas {
+    private int width;
+    private int height;
+    private char[][] matrix;
+
+    public Canvas(int width, int height) {
+        this.width = width;
+        this.height = height;
+        matrix = new char[height + 2][width + 2];
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public char[][] getMatrix() {
+        return matrix;
+    }
+
+    public void setMatrix(char[][] matrix) {
+        this.matrix = matrix;
+    }
+
+    void setPoint(double x, double y, char c) {
+        int rounded_X = (int) Math.round(x);
+        int rounded_Y = (int) Math.round(y);
+
+        if (rounded_X < 0 || rounded_Y < 0 || rounded_Y >= matrix.length || rounded_X >= matrix[0].length) {
+        } else matrix[rounded_Y][rounded_X] = c;
+    }
+
+    void drawMatrix(double x, double y, int[][] matrix, char c) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] != 0) {
+                    setPoint(x + j, y + i, c);
+                }
+            }
+        }
+    }
+}
